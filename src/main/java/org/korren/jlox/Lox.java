eb408e5ec2842581252b1a97f3d331c51ea8042d
Barak Korren <bkorren@redhat.com>
2023-06-30 17:04:26 +0300

    Implement 1-2 character scanner
    Implementing lexical scanner, for now if only
    supports 1-2 character tokens such as `+`,
    `-`, '>=', etc.
    
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- src/main/java/org/korren/jlox/Lox.java
+++ src/main/java/org/korren/jlox/Lox.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
@@ -43,6 +44,12 @@ public class Lox {
     }
 
     private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 
     static void error(int line, String message) {
