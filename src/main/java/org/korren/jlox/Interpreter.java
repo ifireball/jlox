f65da1b80040f852809fb4d42d69777800856566
Barak Korren <bkorren@redhat.com>
2023-08-05 13:02:36 +0300

    While loops!
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- src/main/java/org/korren/jlox/Interpreter.java
+++ src/main/java/org/korren/jlox/Interpreter.java
@@ -224,4 +224,12 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         return null;
     }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
 }
--- src/main/java/org/korren/jlox/Parser.java
+++ src/main/java/org/korren/jlox/Parser.java
@@ -78,13 +78,24 @@ public class Parser {
         }
     }
 
-    // statement -> expressionStatement | ifStatement | printStatement | block
+    // statement -> expressionStatement | ifStatement | printStatement | whileStatement | block
     private Stmt statement() {
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
+        if (match(WHILE)) return whileStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    // whileStatement -> "while" "(" expression ")" statement
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
     }
 
     // ifStatement -> "if" "(" expression ")" statement ( "else" statement )?
