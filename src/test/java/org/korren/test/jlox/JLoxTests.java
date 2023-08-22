9bf21fa0d93162eff7b5984990a081eebaccf5c7
Barak Korren <bkorren@redhat.com>
2023-08-22 23:12:56 +0300

    Test suit (WIP): Samples to tests
    Found out how to convert the list of lox files
    I have into tests. Now need to devise an API to
    run them, read the results, and compare to
    expected values that should be read from
    comments in the same files.
    
    Signed-off-by: Barak Korren <bkorren@redhat.com>

---
+++ src/test/java/org/korren/test/jlox/JLoxTests.java
@@ -0,0 +1,50 @@
+package org.korren.test.jlox;
+
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class JLoxTests {
+    @TestFactory
+    DynamicNode fetchSampleFiles() throws URISyntaxException, IOException {
+        var uri = Objects.requireNonNull(getClass().getResource("samples")).toURI();
+        FileSystem fs;
+        try {
+            fs = FileSystems.getFileSystem(uri);
+        } catch (java.lang.IllegalArgumentException e) {
+            var rootUri = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(),"/",null, null);
+            fs = FileSystems.getFileSystem(rootUri);
+        }
+        var pth = fs.getPath(uri.getPath());
+        return pathToTest(pth);
+    }
+
+    private DynamicNode pathToTest(Path pth) {
+        if (Files.isDirectory(pth)) {
+            try (var files = Files.list(pth)) {
+                var subTests = files
+                        .filter((p) -> Files.isDirectory(p) || p.toString().endsWith("lox"))
+                        .map(this::pathToTest).toArray(DynamicNode[]::new);
+                return dynamicContainer(pth.getFileName().toString(), pth.toUri(), Arrays.stream(subTests));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return dynamicTest(pth.getFileName().toString(), pth.toUri(), () -> {});
+        }
+    }
+}
